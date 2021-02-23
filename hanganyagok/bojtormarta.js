6edf2cde0e9098cce75b6fdd749990c5dab50a15
VeiszB <veiszbetti@gmail.com>
2021-02-23 11:47:08 +0100

    bortorinterju1

--- hanganyagok/bojtormarta.js
+++ hanganyagok/bojtormarta.js
@@ -30,20 +30,40 @@ function createTrackItem(index,name,duration){
   
   var listAudio = [
     {
-      name:"Bojtor Márta interjúja - 01: Gyerekkor, család, sohasem lehet egyedül",
-      file:"http://tarsandor.bochus.hu/hanganyag/gyerekkor.mp3",
+      name:"01: Gyerekkor, család, sohasem lehet egyedül",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmgyerekkor01.mp3",
       duration:"01:25"
     },
     {
-      name:"Bojtor Márta interjúja - 02: Saját otthon, albérlők (Váci Mihály) ",
-      file:"http://tarsandor.bochus.hu/hanganyag/sajatotthon.mp3",
+      name:"02: Saját otthon, albérlők (Váci Mihály) ",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmsajatotthon02.mp3",
       duration:"01:05"
     },
     {
-      name:"Bojtor Márta interjúja - 03: Belülről ismeri a talajvesztettek világát – munkanélküliség",
-      file:"http://tarsandor.bochus.hu/hanganyag/talajvesztettek.mp3",
+      name:"03: Belülről ismeri a talajvesztettek világát – munkanélküliség",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmtalajvesztettek03.mp3",
       duration:"00:41"
-    }
+    },
+    {
+      name:"04: Medicor – munkatársak – 32 év",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmmedicor04.mp3",
+      duration:"00:44"
+    },
+    {
+      name:"05: Irodalomból meg lehet-e élni – díjak – hol publikál",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmirodalombolelni05.mp3",
+      duration:"01:10"
+    },
+    {
+      name:"06: Hogyan érzi magát az irodalmi életben – gyár az közeg volt",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmgyar06.mp3",
+      duration:"02:15"
+    },
+    {
+      name:"07: A munkások világa – hősei tragédiája ",
+      file:"http://tarsandor.bochus.hu/hanganyag/bmhoseitragediaja07.mp3",
+      duration:"03:07"
+    },
   ]
   
   for (var i = 0; i < listAudio.length; i++) {
